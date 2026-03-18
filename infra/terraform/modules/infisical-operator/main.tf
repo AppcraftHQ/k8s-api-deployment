0768eb0f237ed92f12c028614f86e04e573bfb4c
Ademayowa <Ademayowaadeniyi@gmail.com>
2026-03-18 13:36:09 +0100

    feat: test demo workflow before publish

---
+++ infra/terraform/modules/infisical-operator/main.tf
@@ -0,0 +1,11 @@
+
+
+resource "helm_release" "infisical_operator" {
+  name       = "infisical-operator"
+  repository = "https://dl.cloudsmith.io/public/infisical/helm-charts/helm/charts/"
+  chart      = "secrets-operator"
+  namespace  = "infisical-operator-system"
+  version    = var.chart_version
+
+  create_namespace = true
+}
